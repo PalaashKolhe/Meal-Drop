04341d012a4a59ccf9f786bfb99d833ddb44d2cc
Shaishav Shah <shaishav.shah04@gmail.com>
2022-01-15 15:42:38 -0700

    Added auth.

--- server/routes/UserRoutes.js
+++ server/routes/UserRoutes.js
@@ -1,7 +1,11 @@
-const { registerUser } = require("../controllers/UserController");
+const { verify } = require("../controllers/authHelper");
+const { registerUser, loginUser_handler, logoutUser_handler, isAuthUser_handler } = require("../controllers/UserController");
 
 const userRouter = require("express").Router();
 
-userRouter.post("/createUser", registerUser);
+userRouter.post("/create", registerUser);
+userRouter.post("/login", loginUser_handler);
+userRouter.post("/logout", verify, logoutUser_handler);
+userRouter.get("/isAuth", verify, isAuthUser_handler);
 
 module.exports = userRouter;
