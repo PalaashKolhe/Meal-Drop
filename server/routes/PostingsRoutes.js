5b01857349f76f17625a27072e753db134781a02
Palaash Kolhe <65883616+PalaashKolhe@users.noreply.github.com>
2022-01-15 20:56:28 -0700

    got main page and map working. working on creating a posting now

--- server/routes/PostingsRoutes.js
+++ server/routes/PostingsRoutes.js
@@ -1,8 +1,10 @@
-const { createPosting } = require("../controllers/PostingsController");
+const { createPosting, getPostings } = require("../controllers/PostingsController");
 
 const postingRouter = require("express").Router();
 
 postingRouter.post("/create", createPosting);
+postingRouter.get("/all", getPostings);
+
 // userRouter.post("/edit", updateUser);
 
 module.exports = postingRouter;
