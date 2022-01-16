d5a623272470d4a0c70f694324cf978434c23efa
Palaash Kolhe <65883616+PalaashKolhe@users.noreply.github.com>
2022-01-16 12:26:36 -0700

    fixed styling

--- client/src/components/LandingPage/LandingPage.js
+++ client/src/components/LandingPage/LandingPage.js
@@ -4,8 +4,8 @@ import "./LandingPage.css";
 import { Button } from '@mui/material';
 import "../../global.css";
 
-const buttonTheme = {marginRight: "30px", color: "black", border: "2px solid grey"}
-const LinkTheme = {textDecoration: "None", color: "black"};
+const buttonTheme = { marginRight: "30px", color: "black", border: "2px solid grey" }
+const LinkTheme = { textDecoration: "None", color: "black" };
 
 const LandingPage = () => {
     return (
@@ -21,6 +21,8 @@ const LandingPage = () => {
             <div id="text">
                 <h1>MealDrop</h1>
                 <h3>Reducing Food Waste. One Meal at a Time.</h3>
+                <p>Mealdrop is an ingenious application that helps solve food waste absolutely for free! Restaurants with extra food can creating a posting within minutes, detailing the food and pickup options. Once a food bank notices a nearby posting, they can submit a pickup request and collect the extra food whenever they wish.
+                </p>
             </div>
         </div>
     )
